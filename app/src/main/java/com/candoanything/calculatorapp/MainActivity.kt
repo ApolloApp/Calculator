25244fbd3b0d78a4a687c54fbc0bc9cfa67bccc8
emitpool <canalp3rry@gmail.com>
2022-05-04 03:35:50 -0300

    Add Action Clear and put dot in the view

--- app/src/main/java/com/candoanything/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/candoanything/calculatorapp/MainActivity.kt
@@ -3,16 +3,43 @@ package com.candoanything.calculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView?= null
+    var lastNumeric: Boolean = false
+    var lastDot : Boolean = false
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvInput)
     }
 
 
     fun onDigit(view: View){
-        Toast.makeText(this, "Button Clicker", Toast.LENGTH_LONG).show()
+        tvInput?.append((view as Button).text)
+        lastNumeric = true
+
+    }
+
+    fun onClear(view: View){
+        tvInput?.text = ""
+        lastNumeric = false // I make this.
+        lastDot = false // I make this.
+
+    }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){ // no duplicate dot in the view.
+            tvInput?.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
     }
 }
